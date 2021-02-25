fea7542dc2e8bdabcf8902d74ba80a606e2adf8e
omerselimgul <77120893+omerselimgul@users.noreply.github.com>
2021-02-25 21:50:06 +0300

    Add project files.

---
+++ NewUdemy_StopWatch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace NewUdemy_StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool dongu = true;
+            while (dongu)
+            {
+                Console.WriteLine("Başlamak için 'e' tuşana basınız çıkmak için farklı bir tuşa basınız   ");
+                string answer = Console.ReadLine().ToLower();
+                TimeSpan interval = new TimeSpan(0, 0, 0);
+                if (answer == "e")
+                {
+                    var sw = new StopWatch();
+                    var Start = sw.start();
+                    for (int i = 0; i < 1500; i++)
+                    {
+                        Console.WriteLine(TimeSpan.FromSeconds(i + 1));
+                        Thread.Sleep(1000);
+                        devam:
+                        if (Console.KeyAvailable)
+                        {
+                            interval += sw.stop();
+                            Console.WriteLine(interval);
+
+                            Console.ReadKey();
+                            Console.WriteLine("Devam Etmek istermisniz Evet=e  tekrar başlamak için farklı bir tuşa basınız ");
+                            string answer2 = Console.ReadLine();
+                            if (answer2 == "e")
+                            {
+                                Start = sw.start();
+                                goto devam;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                    }
+                }
+                else
+                { dongu = false; }
+                   
+            }
+        }
+    }
+}
---
+++ NewUdemy_StopWatch/StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NewUdemy_StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _starttime { get; set; }
+        private TimeSpan _interval { get; set; }
+
+        public DateTime start()
+        {
+            var starttime = DateTime.Now;
+            _starttime = starttime;
+            return  starttime;
+
+        }
+        public TimeSpan stop()
+        {
+            var interval = DateTime.Now - _starttime;
+            _interval = interval;
+            return _interval;
+        }
+    }
+}
